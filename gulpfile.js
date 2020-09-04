76d1342582976f39a2802ab1e15f155b4b03f9bb
robmc <robert.mcgui@gmail.com>
2020-09-03 22:15:49 -0700

    Isolate post install for development

--- gulpfile.js
+++ gulpfile.js
@@ -13,74 +13,75 @@ const {
     isLinux,
 } = require('.');
 
-// Package tree phase definition
-const taskTreeRoot = defineRoot([
-    definePhase(
-        'verifyPrereqsPhase',
-        ACTIONS.VERIFY,
+// Test phase definitions
+const verifyPrereqsPhase = definePhase(
+    'verifyPrereqsPhase',
+    ACTIONS.VERIFY,
+    [
+        'curl',
+        'git',
+        'node',
+        'npm',
         [
-            'curl',
-            'git',
-            'node',
-            'npm',
-            [
-                'nvm',
-                {
-                    testFn: (pkg) =>
-                        fileExists(
-                            path.join(
-                                process.env['NVM_DIR'] ||
-                                    path.join(
-                                        process.env['HOME'],
-                                        `.${pkg.name}`,
-                                    ),
-                                `${pkg.name}.sh`,
-                            ),
-                        ),
-                },
-            ],
-        ],
-        { parallel: true },
-    ),
-    definePhase('installPythonPhase', ACTIONS.INSTALL, [
-        'python3',
-        [
-            // Required for installing `pip`. Only needed on Linux
-            'python3-distutils',
+            'nvm',
             {
-                skipAction: !isLinux(),
-                testFn: (pkg) =>
-                    !exec(`dpkg -s '${pkg.name}'`, {
-                        silent: true,
-                    }).code,
-            },
-        ],
-        [
-            'pip',
-            {
-                installCommands: [
-                    'sudo curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py',
-                    'sudo -H python3 /tmp/get-pip.py',
-                ],
-            },
-        ],
-        [
-            'pyenv',
-            {
-                installCommands: ['curl https://pyenv.run | bash'],
                 testFn: (pkg) =>
                     fileExists(
                         path.join(
-                            process.env['HOME'],
-                            `.${pkg.name}`,
-                            'bin',
-                            `${pkg.name}`,
+                            process.env['NVM_DIR'] ||
+                                path.join(process.env['HOME'], `.${pkg.name}`),
+                            `${pkg.name}.sh`,
                         ),
                     ),
             },
         ],
-    ]),
-    definePhase('installDotfilesPhase', ACTIONS.INSTALL, [
+    ],
+    { parallel: true },
+);
+
+const installPythonPhase = definePhase('installPythonPhase', ACTIONS.INSTALL, [
+    'python3',
+    [
+        // Required for installing `pip`. Only needed on Linux
+        'python3-distutils',
+        {
+            skipAction: !isLinux(),
+            testFn: (pkg) =>
+                !exec(`dpkg -s '${pkg.name}'`, {
+                    silent: true,
+                }).code,
+        },
+    ],
+    [
+        'pip',
+        {
+            installCommands: [
+                'sudo curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py',
+                'sudo -H python3 /tmp/get-pip.py',
+            ],
+        },
+    ],
+    [
+        'pyenv',
+        {
+            installCommands: ['curl https://pyenv.run | bash'],
+            testFn: (pkg) =>
+                fileExists(
+                    path.join(
+                        process.env['HOME'],
+                        `.${pkg.name}`,
+                        'bin',
+                        `${pkg.name}`,
+                    ),
+                ),
+        },
+    ],
+]);
+
+const installDotfilesPhase = definePhase(
+    'installDotfilesPhase',
+    ACTIONS.INSTALL,
+    [
         [
             // Required for `yadm`
             'envtpl',
@@ -97,9 +98,16 @@ const taskTreeRoot = defineRoot([
                 },
             },
         ],
-    ]),
-]);
+    ],
+);
 
-// Create the full gulp task tree from the package definitions and export them
-// as gulp tasks
-createTaskTree(taskTreeRoot, exports);
+// Create the full gulp task tree from the phase and pakage definitions and
+// export them as gulp tasks
+createTaskTree(
+    defineRoot([
+        // verifyPrereqsPhase,
+        installPythonPhase,
+        installDotfilesPhase,
+    ]),
+    exports,
+);
