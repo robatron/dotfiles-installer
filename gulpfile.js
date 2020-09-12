b3189c1f41d1bfce731f9ec1ff4dde5e1139346c
robmc <robert.mcgui@gmail.com>
2020-09-12 16:51:15 -0700

    Migrate to new package definition

--- gulpfile.js
+++ gulpfile.js
@@ -53,46 +53,34 @@ createTaskTree(
             { parallel: true },
         ),
         definePhase('installUtilities', ACTIONS.INSTALL, [
-            [
-                'coreutils',
-                {
-                    // Mac only. Favor GNU utilities over BSD's
-                    skipAction: !isMac(),
-                },
-            ],
-            'cowsay',
-            [
-                'fortune-mod',
-                {
-                    // Linux version of fortune
-                    skipAction: !isLinux(),
-                },
-            ],
-            [
-                'fortune',
-                {
-                    // Mac version of fortune
-                    skipAction: !isMac(),
-                },
-            ],
-            'gpg',
-            [
-                'gshuf',
-                {
-                    // Symlink shuf to gshuf on Linux to normalize 'shuffle' command
-                    // between Linux and Mac
-                    installCommands: [
-                        `mkdir -p $HOME/bin/`,
-                        'ln -sf `which shuf` $HOME/bin/gshuf',
-                    ],
-                    skipAction: !isLinux(),
-                },
-            ],
-            'vim',
+            p('coreutils', {
+                // Mac only. Favor GNU utilities over BSD's
+                skipAction: !isMac(),
+            }),
+            p('cowsay'),
+            p('fortune-mod', {
+                // Linux version of fortune
+                skipAction: !isLinux(),
+            }),
+            p('fortune', {
+                // Mac version of fortune
+                skipAction: !isMac(),
+            }),
+            p('gpg'),
+            p('gshuf', {
+                // Symlink shuf to gshuf on Linux to normalize 'shuffle' command
+                // between Linux and Mac
+                installCommands: [
+                    `mkdir -p $HOME/bin/`,
+                    'ln -sf `which shuf` $HOME/bin/gshuf',
+                ],
+                skipAction: !isLinux(),
+            }),
+            p('vim'),
         ]),
         definePhase('installPythonPhase', ACTIONS.INSTALL, [
-            'python3',
-            [
+            p('python3'),
+            p(
                 // Required for installing `pip`. Only needed on Linux
                 'python3-distutils',
                 {
@@ -102,112 +90,88 @@ createTaskTree(
                             silent: true,
                         }).code,
                 },
-            ],
-            [
-                'pip',
-                {
-                    installCommands: [
-                        'sudo curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py',
-                        'sudo -H python3 /tmp/get-pip.py',
-                    ],
-                },
-            ],
-            [
-                'pyenv',
-                {
-                    installCommands: ['curl https://pyenv.run | bash'],
-                    testFn: (pkg) =>
-                        fileExists(
-                            path.join(
-                                process.env['HOME'],
-                                `.${pkg.name}`,
-                                'bin',
-                                `${pkg.name}`,
-                            ),
+            ),
+            p('pip', {
+                installCommands: [
+                    'sudo curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py',
+                    'sudo -H python3 /tmp/get-pip.py',
+                ],
+            }),
+            p('pyenv', {
+                installCommands: ['curl https://pyenv.run | bash'],
+                testFn: (pkg) =>
+                    fileExists(
+                        path.join(
+                            process.env['HOME'],
+                            `.${pkg.name}`,
+                            'bin',
+                            `${pkg.name}`,
                         ),
-                },
-            ],
-            [
+                    ),
+            }),
+            p(
                 // Required for `yadm`
                 'envtpl',
                 {
                     installCommands: ['sudo -H pip install envtpl'],
                 },
-            ],
+            ),
         ]),
         definePhase('installTerminalPhase', ACTIONS.INSTALL, [
-            'zsh',
-            [
-                'oh-my-zsh',
-                {
-                    installCommands: [
-                        `wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O /tmp/omzshinstall.sh`,
-                        `RUNZSH=no sh /tmp/omzshinstall.sh`,
-                    ],
-                    testFn: (pkg) => fileExists(OMZDir),
-                },
-            ],
-            [
-                'spaceship-prompt',
-                {
-                    installCommands: [
-                        `git clone https://github.com/denysdovhan/spaceship-prompt.git ${SpaceshipThemeDir}`,
-                        `ln -s "${SpaceshipThemeDir}/spaceship.zsh-theme" "${OMZDir}/themes/spaceship.zsh-theme"`,
-                    ],
-                    testFn: (pkg) => fileExists(SpaceshipThemeDir),
-                },
-            ],
-            [
-                'powerline',
-                {
-                    installCommands: [
-                        `git clone https://github.com/powerline/fonts.git ${powerlineDir}`,
-                        `ls -lah ${powerlineDir}`,
+            p('zsh'),
+            p('oh-my-zsh', {
+                installCommands: [
+                    `wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O /tmp/omzshinstall.sh`,
+                    `RUNZSH=no sh /tmp/omzshinstall.sh`,
+                ],
+                testFn: (pkg) => fileExists(OMZDir),
+            }),
+            p('spaceship-prompt', {
+                installCommands: [
+                    `git clone https://github.com/denysdovhan/spaceship-prompt.git ${SpaceshipThemeDir}`,
+                    `ln -s "${SpaceshipThemeDir}/spaceship.zsh-theme" "${OMZDir}/themes/spaceship.zsh-theme"`,
+                ],
+                testFn: (pkg) => fileExists(SpaceshipThemeDir),
+            }),
+            p('powerline', {
+                installCommands: [
+                    `git clone https://github.com/powerline/fonts.git ${powerlineDir}`,
+                    `ls -lah ${powerlineDir}`,
 
-                        // Directory needs to exist and be owned by user to install
-                        `mkdir -p $HOME/.local`,
-                        `sudo chown -R $USER: $HOME/.local`,
-                        `${powerlineDir}/install.sh`,
-                    ],
-                    testFn: (pkg) => fileExists(powerlineDir),
-                },
-            ],
+                    // Directory needs to exist and be owned by user to install
+                    `mkdir -p $HOME/.local`,
+                    `sudo chown -R $USER: $HOME/.local`,
+                    `${powerlineDir}/install.sh`,
+                ],
+                testFn: (pkg) => fileExists(powerlineDir),
+            }),
 
-            'tmux',
-            [
-                'reattach-to-user-namespace',
-                {
-                    // Mac only. Required for tmux to interface w/ OS X clipboard, etc.
-                    skipAction: !isMac(),
-                },
-            ],
+            p('tmux'),
+            p('reattach-to-user-namespace', {
+                // Mac only. Required for tmux to interface w/ OS X clipboard, etc.
+                skipAction: !isMac(),
+            }),
         ]),
         definePhase('installDotfilesPhase', ACTIONS.INSTALL, [
-            [
-                'yadm',
-                {
-                    gitPackage: {
-                        binSymlink: 'yadm',
-                        repoUrl: 'https://github.com/TheLocehiliosan/yadm.git',
-                    },
+            p('yadm', {
+                gitPackage: {
+                    binSymlink: 'yadm',
+                    repoUrl: 'https://github.com/TheLocehiliosan/yadm.git',
                 },
-            ],
-            [
-                'dotfiles',
-                {
-                    installCommands: [
-                        `${path.join(
-                            binInstallDir,
-                            'yadm',
-                        )} clone ${dotfilesRepoUrl}`,
-                    ],
-                    // This step requires user interaction (entering a
-                    // password), so skip it if we're in a continuous-
-                    // delivery environment (GitHub Actions)
-                    skipAction: process.env['CI'],
-                    testFn: (pkg) => fileExists(dotfilesRepoDir),
-                },
-            ],
+            }),
+            p('dotfiles', {
+                installCommands: [
+                    `${path.join(
+                        binInstallDir,
+                        'yadm',
+                    )} clone ${dotfilesRepoUrl}`,
+                ],
+                // This step requires user interaction (entering a
+                // password), so skip it if we're in a continuous-
+                // delivery environment (GitHub Actions)
+                skipAction: process.env['CI'],
+                testFn: (pkg) => fileExists(dotfilesRepoDir),
+            }),
         ]),
         definePhase(
             'installMacGuiApps',
@@ -219,13 +183,12 @@ createTaskTree(
                 'keepingyouawake',
                 'spectacle',
                 'visual-studio-code',
-            ].map((pkg) => [
-                pkg,
-                {
-                    installCommands: [`brew cask install ${pkg}`],
+            ].map((name) =>
+                p(name, {
+                    installCommands: [`brew cask install ${name}`],
                     skipAction: !isMac(),
-                },
-            ]),
+                }),
+            ),
         ),
     ]),
     exports,
