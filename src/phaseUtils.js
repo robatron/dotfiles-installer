1ca292b41662a7a99b5b40b017459b31ecafa17b
robmc <robert.mcgui@gmail.com>
2020-09-12 20:00:18 -0700

    Inject verifyPrereqsPhase into the defineRoot function

--- src/phaseUtils.js
+++ src/phaseUtils.js
@@ -14,12 +14,40 @@ const definePhase = (name, action, targets, opts = {}) => [
 ];
 
 // Helper function to create the root phase definition with required parameters
-// and structure
+// and structure. Inject `verifyPrereqsPhase` as the first phase on the root.
 const defineRoot = (targets, parallel = false) => [
-    definePhase(PHASE_NAME_DEFAULT, ACTIONS.RUN_PHASES, targets, {
-        parallel,
-    }),
+    definePhase(
+        PHASE_NAME_DEFAULT,
+        ACTIONS.RUN_PHASES,
+        [verifyPrereqsPhase, ...targets],
+        {
+            parallel,
+        },
+    ),
 ];
+
+// Phase for verifying akinizer prerequisites are installed
+const verifyPrereqsPhase = definePhase(
+    'verifyPrereqsPhase',
+    ACTIONS.VERIFY,
+    [
+        p('curl'),
+        p('git'),
+        p('node'),
+        p('npm'),
+        p('nvm', {
+            testFn: (pkg) =>
+                fileExists(
+                    path.join(
+                        process.env['NVM_DIR'] ||
+                            path.join(process.env['HOME'], `.${pkg.name}`),
+                        `${pkg.name}.sh`,
+                    ),
+                ),
+        }),
+    ],
+    { parallel: true },
+);
 
 module.exports = {
     definePackage,
